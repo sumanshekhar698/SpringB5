e09994a36c8c2ecb3a20af7a5bf4fed9e6764dd1
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2023-05-13 01:46:36 +0530

    Initial commit

---
+++ src/main/java/com/springb5/basics/Main.java
@@ -0,0 +1,41 @@
+package com.springb5.basics;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// UPSC
+//		Student s1 = new Student();// OLC Object Life Cycle
+//		s1.setAge(23);
+//		s1.setName("Jack Sparow");
+//		System.out.println(s1);
+//		s1 = null;
+
+//				Spring beans are POJOs managed by spring
+		String path = "com/springb5/basics/config.xml";
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(path);
+
+		Student s2 = (Student) context.getBean("s2");
+		System.out.println(s2);//IOC
+
+		
+		//Spring Infosys - 2.8 Lac
+		// 10Lac
+		//25Lacs
+		
+		Student s3 = (Student) context.getBean("s3");
+		System.out.println(s3.hashCode());
+		s3.setAge(100);
+		System.out.println(s3);
+		
+//		Student s4 = (Student) context.getBean("s4");//Dont ask a bean which you have not defined
+		
+		Student s3_2nd = (Student) context.getBean("s3");
+		System.out.println(s3_2nd.hashCode());//By default Spring follows Singelton Pattern
+
+
+	}
+
+}
---
+++ src/main/java/com/springb5/basics/Student.java
@@ -0,0 +1,44 @@
+package com.springb5.basics;
+
+public class Student {// POJO Class -Plain Old Java Object
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+		System.out.println("Student parameterized () called");
+	}
+
+	public void setName(String name) {//By default Spring does Setter Injection
+		System.out.println("setName() called");
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		System.out.println("setAge() called");
+
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + "]";
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("Student () called");
+	}
+
+}
